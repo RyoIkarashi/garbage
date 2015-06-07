fc284801d8146587ed73529d36557414c7e59bbd
Ryo Ikarashi <ryo.5630@gmail.com>
2015-06-07 13:40:51 +0900

    add auto-redirection and a button to redirect

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,10 +1,27 @@
 var React  = require('react');
 
 module.exports = React.createClass({
+
+  redirectToHP() {
+    window.location.replace('/');
+  },
+
+  componentDidMount() {
+    // this.interval = setInterval(this.redirectToHP, 5000);
+  },
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  },
+
   render() {
     return (
-      <div className="notFound404">
-        <p>Sorry but I can't find what you're looing for... m(__)m</p>
+      <div className="not-found-404">
+        <div className="inner">
+          <p className="not-found-wording--main">Page Not Found!</p>
+          <p className="not-found-wording--sub">Redirecting to homepage after 5 sec or just click the button below.</p>
+          <button onClick={this.redirectToHP} className="redirect-btn">Back to Home</button>
+        </div>
       </div>
     );
   }
