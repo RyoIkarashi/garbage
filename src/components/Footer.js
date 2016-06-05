6c669bef2212af0218dcec6e92648a9ebb750097
Ryo Ikarashi <ryo.5630@gmail.com>
2016-06-05 16:07:32 +0900

    add a github repo link for this site

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,7 @@ export default class Footer extends Component {
       <footer className="footer clearfix" role="contentinfo">
         <small className="copyright">
           <span className="footer__top--xs">&copy; {year} All Rights Reserved | </span>
-          <span className="foote__bottom--xs"><em>Garbage</em> created by <a href="https://github.com/RyoIkarashi/garbage" className="name-name"><i className="fa fa-github"></i> <strong>Ryo Ikarashi</strong></a></span>
+          <span className="foote__bottom--xs"><em><a href="https://github.com/RyoIkarashi/garbage">Garbage</a></em> created by <a href="https://github.com/RyoIkarashi/garbage" className="name-name"><i className="fa fa-github"></i> <strong>Ryo Ikarashi</strong></a></span>
         </small>
       </footer>
     );
