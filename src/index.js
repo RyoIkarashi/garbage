21fa7403628658be784e996c4fd96d51d35d0cc9
Ryo Ikarashi <ryo.5630@gmail.com>
2016-07-09 16:50:45 +0900

    add styles to entry index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import 'babel-polyfill';
 
+import './styles/main.css';
+
 import { render } from 'react-dom';
 import Root from './components/Root';
 import configureStore from './store/configureStore';
