3499b0a28a29b0f5d2ba88f0f1969ed4745ad1b5
Ryo Ikarashi <ryo.5630@gmail.com>
2016-07-17 22:58:26 +0900

    add yoast seo for rest api

--- dist/theme/functions.php
+++ dist/theme/functions.php
@@ -17,3 +17,37 @@ add_filter('json_query_vars', function ($vars) {
     $vars[] = 'date_query';
     return $vars;
 });
+
+add_action( 'rest_api_init', function() {
+ register_api_field( 'post',
+    'yoast',
+    array(
+       'get_callback'    => 'get_yoast',
+       'update_callback' => null,
+       'schema'          => null,
+    )
+ );
+});
+
+function get_yoast( $object, $field_name, $request ) {
+  $yoastMeta = array(
+    'yoast_wpseo_title' => get_post_meta($object['id'], '_yoast_wpseo_title', true),
+    'yoast_wpseo_metadesc' => get_post_meta($object['id'], '_yoast_wpseo_metadesc', true),
+    'yoast_wpseo_focuskw' => get_post_meta($object['id'], '_yoast_wpseo_focuskw', true),
+    'yoast_wpseo_linkdex' => get_post_meta($object['id'], '_yoast_wpseo_linkdex', true),
+    'yoast_wpseo_metakeywords' => get_post_meta($object['id'], '_yoast_wpseo_metakeywords', true),
+    'yoast_wpseo_meta-robots-noindex' => get_post_meta($object['id'], '_yoast_wpseo_meta-robots-noindex', true),
+    'yoast_wpseo_meta-robots-nofollow' => get_post_meta($object['id'], '_yoast_wpseo_meta-robots-nofollow', true),
+    'yoast_wpseo_meta-robots-adv' => get_post_meta($object['id'], '_yoast_wpseo_meta-robots-adv', true),
+    'yoast_wpseo_canonical' => get_post_meta($object['id'], '_yoast_wpseo_canonical', true),
+    'yoast_wpseo_redirect' => get_post_meta($object['id'], '_yoast_wpseo_redirect', true),
+    'yoast_wpseo_opengraph-title' => get_post_meta($object['id'], '_yoast_wpseo_opengraph-title', true),
+    'yoast_wpseo_opengraph-description' => get_post_meta($object['id'], '_yoast_wpseo_opengraph-description', true),
+    'yoast_wpseo_opengraph-image' => get_post_meta($object['id'], '_yoast_wpseo_opengraph-image', true),
+    'yoast_wpseo_twitter-title' => get_post_meta($object['id'], '_yoast_wpseo_twitter-title', true),
+    'yoast_wpseo_twitter-description' => get_post_meta($object['id'], '_yoast_wpseo_twitter-description', true),
+    'yoast_wpseo_twitter-image' => get_post_meta($object['id'], '_yoast_wpseo_twitter-image', true)
+  );
+
+  return $yoastMeta;
+}
