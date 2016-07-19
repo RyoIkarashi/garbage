7c7a0ecdcc351fc822d213363b57a8eb8e9d141c
Ryo Ikarashi <ryo.5630@gmail.com>
2016-07-19 13:18:32 +0900

    add files for production

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,36 +1,7 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import { browserHistory } from 'react-router';
-import createSagaMiddleware, { END } from 'redux-saga';
-import { routerMiddleware } from 'react-router-redux';
-import createLogger from 'redux-logger';
-import rootReducer from '../reducers';
-import DevTools from '../containers/DevTools'
+/* eslint-disable no-undef, no-process-env */
 
-const configureStore = (initialState = {}) => {
-
-  const sagaMiddleware = createSagaMiddleware();
-
-  const enhancer = compose(
-    applyMiddleware(
-      sagaMiddleware,
-      routerMiddleware(browserHistory),
-      createLogger()
-    ),
-    DevTools.instrument()
-  );
-
-  const store = createStore(rootReducer, initialState, enhancer);
-
-  if (module.hot) {
-    module.hot.accept('../reducers', () => {
-      const nextRootReducer = require('../reducers').default;
-      store.replaceReducer(nextRootReducer);
-    });
-  }
-
-  store.runSaga = sagaMiddleware.run;
-  store.close = () => store.dispatch(END);
-  return store;
-};
-
-export default configureStore;
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
+}
